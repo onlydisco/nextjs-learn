21f4c40b09bfa353b9654372cac2f251cc362fe5
Sergey Mukhin <onlydisco@yandex.ru>
2024-08-28 00:38:36 +0500

    feat: dashboard data fetching

--- nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
+++ nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
@@ -20,7 +20,6 @@ export default async function RevenueChart({ revenue }: { revenue: Revenue[] }) 
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Recent Revenue</h2>
-      NOTE: Uncomment this code in Chapter 7
       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
           <div
